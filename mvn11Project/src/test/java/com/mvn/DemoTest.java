eec5edc94457fd5d385dc9ae8e926bd615859689
Hp <Hp@192.168.2.10>
2018-06-26 00:02:11 +0530

    First commit

---
+++ mvn11Project/src/test/java/com/mvn/DemoTest.java
@@ -0,0 +1,18 @@
+package com.mvn;
+
+import org.testng.annotations.Test;
+
+public class DemoTest 
+{
+	@Test
+	public void DemoTest1()
+	{
+		System.out.println("executing TEst1");
+	}
+	@Test
+	public void DemoTest2()
+	{
+		System.out.println("executing TEst2");
+	}
+
+}
